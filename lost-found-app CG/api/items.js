7c1aaff7f6e2cd60968be67444ae186f61c0d7d4
terimatakli123 <131887202+terimatakli123@users.noreply.github.com>
2025-07-14 18:44:26 +0530

    Delete lost-found-app CG directory

--- lost-found-app CG/api/items.js
+++
@@ -1,9 +0,0 @@
-let items = [];
-
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    return res.status(200).json({ items });
-  } else {
-    res.status(405).json({ error: 'Method not allowed' });
-  }
-}
